ac49573bc8015d2881711b8b183efa5a120fa1b3
BladeRunner-A2C <john.smith@unused.email>
2023-06-29 00:52:27 +0600

    miatoll-miuicamera: Update for new paths
    Signed-off-by: BladeRunner-A2C <john.smith@unused.email>

--- miuicamera.mk
+++ miuicamera.mk
@@ -1,11 +1,9 @@
 #
 # Copyright (C) 2023 The LineageOS Project
+#           (C) 2023 Paranoid Android
 #
 # SPDX-License-Identifier: Apache-2.0
 #
-
-# Face recognition (Megvii or SenseTime)
-$(call inherit-product-if-exists, vendor/xiaomi/proprietary/proprietary-vendor.mk)
 
 # Prebuilt APKs
 PRODUCT_PACKAGES += \
@@ -19,7 +17,11 @@ PRODUCT_DEXPREOPT_SPEED_APPS += \
 # Properties
 PRODUCT_PRODUCT_PROPERTIES += \
     persist.vendor.camera.privapp.list=com.android.camera \
+    ro.com.google.lens.oem_camera_package=com.android.camera \
     ro.miui.build.region=in \
     ro.miui.notch=1 \
     ro.product.mod_device=_global \
-    ro.com.google.lens.oem_camera_package=com.android.camera
+
+# Soong namespaces
+PRODUCT_SOONG_NAMESPACES += \
+    $(LOCAL_PATH)
